f8cb3023bfaa1cb7b560f3ba9df62720f2278b42
Yan Minagawa <yan@minagawa.de>
2026-04-05 08:38:52 +0700

    chore: update formula with real sha256 for v0.1.1.0

--- Formula/manifestor.rb
+++ Formula/manifestor.rb
@@ -1,8 +1,8 @@
 class Manifestor < Formula
   desc "Grab the latest file from ~/Downloads with a clean name"
   homepage "https://github.com/yncyrydybyl/manifestor"
-  url "https://github.com/yncyrydybyl/manifestor/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "PUT_REAL_SHA256_HERE"
+  url "https://github.com/yncyrydybyl/manifestor/archive/refs/tags/v0.1.1.0.tar.gz"
+  sha256 "35472166abc15d4048f5890a26dccd3d6101f3aac67ce35be9770046993d7b62"
   license "MIT"
 
   depends_on "go" => :build
